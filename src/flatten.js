df0db73570a5eb69748052961dad1ab053229274
Chris Geirman <chris@geirman.com>
2016-03-22 19:19:53 -0700

    added deletable comment

--- src/flatten.js
+++ src/flatten.js
@@ -3,6 +3,8 @@ export default flatten
 function flatten(arr) {
   return arr.reduce(function flattenReducer(flat, toFlatten) {
     return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten)
+// I'd like to understand what happens when I push my PR up, rename my branch,
+// then push again.
   }, [])
 }
 
